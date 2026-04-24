35a0165ed178fc8101d3f658a678badb98916e5f
ProssimalCrost <theylon.silva@hotmail.com>
2026-04-24 12:00:21 -0300

    Aprimorando o método ListAsync para melhor filtragem de profissionais e atualização de arquivos binários

--- back/obj/Debug/net8.0/MatchJob.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/MatchJob.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchJob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b2bff24e4bc8a020c35d6205d2f8079648fb1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchJob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchJob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
